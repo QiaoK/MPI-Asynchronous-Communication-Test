1b7e6d56504b50dd01573fe7a35cfd76ce27bf9b
Qiao Kang <qiao.kang@eecs.northwestern.edu>
2020-01-05 13:20:12 -0600

    test

--- mpi_test.c
+++ mpi_test.c
@@ -100,18 +100,18 @@ int all_to_many_striped(int rank, int isagg, int procs, int cb_nodes, int proc_n
         // If the maximum communication size is greater than the number of processes, we just run many-to-all communication directly.
         start = MPI_Wtime();
         j = 0;
-        for ( i = 0; i < cb_nodes; ++i ){
-            temp = (rank + i)%cb_nodes;
-            MPI_Issend(send_buf[temp], data_size, MPI_BYTE, rank_list[temp], rank + rank_list[temp], MPI_COMM_WORLD, &requests[j++]);
-        }
         if (isagg) {
             for ( i = 0; i < cb_nodes; ++i ){
-                for ( x = (myindex - i + cb_nodes) % cb_nodes; x < procs; x+=cb_nodes ){
+                for ( x = (myindex + i) % cb_nodes; x < procs; x+=cb_nodes ){
                     temp = x;
                     MPI_Irecv(recv_buf[temp], data_size, MPI_BYTE, temp, rank + temp, MPI_COMM_WORLD, &requests[j++]);
                 }
                 
             }
+        }
+        for ( i = 0; i < cb_nodes; ++i ){
+            temp = (rank - i + cb_nodes) % cb_nodes;
+            MPI_Issend(send_buf[temp], data_size, MPI_BYTE, rank_list[temp], rank + rank_list[temp], MPI_COMM_WORLD, &requests[j++]);
         }
         timer->post_request_time += MPI_Wtime() - start;
         if (j) {
