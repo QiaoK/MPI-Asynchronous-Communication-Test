5c399b4de00fe622920283dc642a3358940288fe
Qiao Kang <qiao.kang@eecs.northwestern.edu>
2020-01-08 00:26:57 -0600

    variable message size patch

--- mpi_test.c
+++ mpi_test.c
@@ -126,7 +126,7 @@ int prepare_all_to_many_data(char ***send_buf, char*** recv_buf, MPI_Status **st
 
         recv_buf[0][0] = (char*) malloc(sizeof(char) * r_len * procs);
         if (rank == 0){
-            printf("%d,%d\n", rank, (int)(recv_buf[0][0]));
+            printf("%d,%lld\n", r_len, (long long int)(recv_buf[0][0]));
         }
         for ( i = 1; i < procs; ++i ){
             recv_buf[0][i] = recv_buf[0][i-1] + r_lens[0][i-1];
