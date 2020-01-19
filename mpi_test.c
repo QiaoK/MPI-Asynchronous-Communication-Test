92d2cbbf6bb80f280fda71375c7fdb4f683d1479
Qiao Kang <qiao.kang@eecs.northwestern.edu>
2020-01-18 19:41:25 -0600

    test

--- mpi_test.c
+++ mpi_test.c
@@ -225,7 +225,7 @@ int clean_all_to_many(int rank, int procs, int cb_nodes, int *rank_list, int myi
 int all_to_many_tam(int rank, int isagg, int procs, int cb_nodes, int data_size, int *rank_list, int comm_size, int procs_node, Timer *timer, int iter, int ntimes){
     double total_start;
     int i, m, myindex = 0, s_len, *r_lens;
-    int *node_size, *local_ranks, *global_receivers, *process_node_list, nrecvs/*, is_aggregator_new, local_aggregator_size, *aggregator_local_ranks, *process_aggregator_list, *local_aggregators, nprocs_aggregator*/;
+    int *node_size, *local_ranks, *global_receivers, *process_node_list, nrecvs;
     char **send_buf, **send_buf2;
     char **recv_buf = NULL;
     int *sendcounts = NULL, *recvcounts = NULL, *sdispls = NULL, *rdispls = NULL;
@@ -273,8 +273,12 @@ int all_to_many_tam(int rank, int isagg, int procs, int cb_nodes, int data_size,
     for (i=0; i<procs; i++) dtypes[i] = MPI_BYTE;
 
     static_node_assignment(rank, procs, 0, &procs_node, &nrecvs, &node_size, &local_ranks, &global_receivers, &process_node_list);
-    //aggregator_meta_information(rank, process_node_list, procs, nrecvs, cb_nodes, rank_list, 1, &is_aggregator_new, &local_aggregator_size, &local_aggregators, &nprocs_aggregator, &aggregator_local_ranks, &process_aggregator_list, 0);
-    collective_write(rank, procs, procs_node, nrecvs, local_ranks, global_receivers, process_node_list, recvcounts, sendcounts, recv_buf, send_buf2, iter, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    total_start = MPI_Wtime();
+    for ( m = 0; m < ntimes; ++m ){
+        collective_write(rank, procs, procs_node, nrecvs, local_ranks, global_receivers, process_node_list, recvcounts, sendcounts, recv_buf, send_buf2, iter, MPI_COMM_WORLD);
+    }
 
     comm_size = procs;
 
