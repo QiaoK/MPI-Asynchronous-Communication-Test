68590627038ec563aecb2cde3e87f9277ed115eb
Qiao Kang <qiao.kang@eecs.northwestern.edu>
2020-01-27 15:24:41 -0600

    test

--- mpi_test.c
+++ mpi_test.c
@@ -1739,7 +1739,7 @@ int main(int argc, char **argv){
         }
     }
     create_aggregator_list(rank, procs, cb_nodes, proc_node, aggregator_type, &rank_list, &isagg);
-/*
+
     if (rank == 0){
         printf("total number of processes = %d, cb_nodes = %d, proc_node = %d, data size = %d, comm_size = %d, ntimes=%d\n", procs, cb_nodes, proc_node, data_size, comm_size, ntimes);
 
@@ -1750,7 +1750,7 @@ int main(int argc, char **argv){
         printf("\n");
 
     }
-*/
+
     for ( i = 0; i < iter; ++i ){
         if (method == 0 || method == 1){
             all_to_many(rank, isagg, procs, cb_nodes, data_size, rank_list, comm_size, &timer1, i, ntimes);
